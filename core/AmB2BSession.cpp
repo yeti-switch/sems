25aae38640febb4af109c187b965266dcd10b23a
Michael Furmur <m.furmur@gmail.com>
2017-06-21 16:41:07 +0300

    AmB2BSession: updateLocalBody: fix processing of the zero-length SDP body

--- core/AmB2BSession.cpp
+++ core/AmB2BSession.cpp
@@ -520,6 +520,11 @@ void AmB2BSession::updateLocalBody(AmMimeBody& body)
   AmMimeBody *sdp = body.hasContentType(SIP_APPLICATION_SDP);
   if (!sdp) return;
 
+  if(!sdp->getLen()) {
+      sdp->clear();
+      return;
+  }
+
   AmSdp parser_sdp;
   if (parser_sdp.parse((const char*)sdp->getPayload())) {
     DBG("SDP parsing failed!\n");
