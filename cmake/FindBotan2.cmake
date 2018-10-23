ab88ad01560f9c6ef48ac63c09541ca9be3a120c
Michael Furmur <m.furmur@gmail.com>
2018-10-23 16:41:10 +0300

    FindBotan2.cmake: fix comments

--- cmake/FindBotan2.cmake
+++ cmake/FindBotan2.cmake
@@ -1,24 +1,25 @@
 #.rst:
-# FindConfuse
+# FindBotan2
 # --------
 #
-# Find Confuse
+# Find Botan2
 #
-# Find Confuse headers and libraries.
+# Find Botan2 headers and libraries.
 #
 # ::
 #
-#   BOTAN2_LIBRARIES      - List of libraries when using Confuse.
-#   BOTAN2_INCLUDE_DIRS   - List of include directories when using Confuse.
-#   BOTAN2_FOUND          - True if libconfuse found.
-#   BOTAN2_VERSION        - Version of found libconfuse.
+#   BOTAN2_LIBRARIES      - List of libraries when using Botan2.
+#   BOTAN2_INCLUDE_DIRS   - List of include directories when using Botan2
+#   BOTAN2_FOUND          - True if libbotan2 found.
+#   BOTAN2_VERSION        - Version of found libbotan2.
 
 find_package(PkgConfig REQUIRED)
 pkg_check_modules(BOTAN2 botan-2)
 
-# handle the QUIETLY and REQUIRED arguments and set Confuse_FOUND to TRUE if
+# handle the QUIETLY and REQUIRED arguments and set BOTAN2_FOUND to TRUE if
 # all listed variables are TRUE
 include(FindPackageHandleStandardArgs)
-FIND_PACKAGE_HANDLE_STANDARD_ARGS(BOTAN2
-                                  REQUIRED_VARS BOTAN2_LIBRARIES BOTAN2_INCLUDE_DIRS
-				  VERSION_VAR BOTAN2_VERSION)
+FIND_PACKAGE_HANDLE_STANDARD_ARGS(
+    BOTAN2
+    REQUIRED_VARS BOTAN2_LIBRARIES BOTAN2_INCLUDE_DIRS
+    VERSION_VAR BOTAN2_VERSION)
