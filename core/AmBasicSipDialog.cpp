095fe62dfc8eb8765354e92e761dfcc46cdbc651
Michael Furmur <m.furmur@gmail.com>
2019-01-16 18:24:11 +0200

    adjust loglevels for failed resolving, registrations, sendRequest

--- core/AmBasicSipDialog.cpp
+++ core/AmBasicSipDialog.cpp
@@ -889,7 +889,7 @@ int AmBasicSipDialog::sendRequest(const string& method,
                    logger,sensor,timers_override,
 				   redirects_allowed);
   if(res) {
-    WARN("Could not send request: method=%s; ruri=%s; call-id=%s; cseq=%i\n",
+    DBG("Could not send request: method=%s; ruri=%s; call-id=%s; cseq=%i\n",
       req.method.c_str(),
       req.r_uri.c_str(),
       req.callid.c_str(),
