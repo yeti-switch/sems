627531e82d860d0bd75e6ef40d83edc1e38df973
Michael Furmur <m.furmur@gmail.com>
2019-05-20 16:10:18 +0300

    pcap_logger: fix methods args type. fixes build

--- core/sip/pcap_logger.cpp
+++ core/sip/pcap_logger.cpp
@@ -120,7 +120,7 @@ int pcap_logger::log(const char* buf, int len,
     return 0;
 }
 
-int pcap_logger::logv4(const char *data, int data_len, struct sockaddr *src, struct sockaddr *dst, size_t addr_len, struct timeval &event_time)
+int pcap_logger::logv4(const char *data, int data_len, struct sockaddr_storage *src, struct sockaddr_storage *dst, size_t addr_len, struct timeval &event_time)
 {
   if (((sockaddr_in*)src)->sin_family != AF_INET) {
     ERROR("writing only IPv4 is supported\n");
@@ -173,7 +173,7 @@ int pcap_logger::logv4(const char *data, int data_len, struct sockaddr *src, str
   return 0;
 }
 
-int pcap_logger::logv6(const char *data, int data_len, struct sockaddr *src, struct sockaddr *dst, size_t addr_len, struct timeval &event_time)
+int pcap_logger::logv6(const char *data, int data_len, struct sockaddr_storage *src, struct sockaddr_storage *dst, size_t addr_len, struct timeval &event_time)
 {
   if (((sockaddr_in*)src)->sin_family != AF_INET6) {
     ERROR("writing only IPv6 is supported\n");
