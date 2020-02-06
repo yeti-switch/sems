7d81f14e9af56554bed3bf12254980c3cae67f4c
alexey <alexey@debian.avolokitin>
2020-02-06 13:07:57 +0200

    change findcurl cmake script

--- cmake/FindCurl.cmake
+++ cmake/FindCurl.cmake
@@ -4,13 +4,13 @@
 #
 # ::
 #
-#   CURL_INCLUDE_DIRS   - where to find curl.h
+#   CURL_INCLUDE_DIR   - where to find curl.h
 #   CURL_LIBRARIES      - List of libraries when using libcurl
 #   CURL_FOUND          - True if libyeticc found.
 #   CURL_VERSION        - Version of found libcurl.
 
 find_package(PkgConfig REQUIRED)
-pkg_check_modules(CURL curl)
+pkg_check_modules(CURL libcurl)
 
 # handle the QUIETLY and REQUIRED arguments and set OPUS_FOUND to TRUE if
 # all listed variables are TRUE
