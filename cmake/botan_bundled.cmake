0fc0b4f0e9c91c6daed5b884e7064a84e150ca81
Michael Furmur <furmur@pm.me>
2021-09-14 00:54:30 +0300

    libbotan: add TNAuthList support

--- cmake/botan_bundled.cmake
+++ cmake/botan_bundled.cmake
@@ -1,7 +1,7 @@
 MESSAGE(STATUS "Enable building of the bundled libbotan")
 
 set (BOTAN_DIR third/botan)
-set (BOTAN_PATCH_FILE ${PROJECT_SOURCE_DIR}/third/botan_patch.diff)
+set (BOTAN_PATCH_FILE ${PROJECT_SOURCE_DIR}/third/botan.patch)
 set (BOTAN_SRC_DIR ${PROJECT_SOURCE_DIR}/${BOTAN_DIR})
 set (BOTAN_BIN_DIR ${PROJECT_BINARY_DIR}/${BOTAN_DIR})
 set (BOTAN_BUNDLED_LIB ${BOTAN_BIN_DIR}/libbotan-2.a)
@@ -14,6 +14,7 @@ add_custom_target(libbotan ALL DEPENDS ${BOTAN_BUNDLED_LIB})
 IF(NOT EXISTS ${BOTAN_BIN_DIR}/configure_stdout)
     file(MAKE_DIRECTORY ${BOTAN_BIN_DIR})
     execute_process(COMMAND ${CMAKE_COMMAND} -E copy_directory ${BOTAN_SRC_DIR} ${BOTAN_BIN_DIR})
+    execute_process(COMMAND patch --strip=1 --directory=${BOTAN_BIN_DIR} --input=${BOTAN_PATCH_FILE})
     execute_process(COMMAND ./configure.py ${BOTAN_CONFIG_ARGS} OUTPUT_FILE configure_stdout WORKING_DIRECTORY ${BOTAN_BIN_DIR})
 ENDIF(NOT EXISTS ${BOTAN_BIN_DIR}/configure_stdout)
 
