5634df708a5d29e26eedfcf5d2195e164082945a
Michael Furmur <furmur@pm.me>
2023-03-08 13:23:45 +0200

    sip_parser_async: fix identation

--- core/sip/sip_parser_async.cpp
+++ core/sip/sip_parser_async.cpp
@@ -11,34 +11,30 @@ using std::string;
 
 static int skip_line_async(parser_state* pst, char* end)
 {
-  char*& c = pst->c;
-  int& st = pst->st;
-  int& saved_st = pst->saved_st;
+    char*& c = pst->c;
+    int& st = pst->st;
+    int& saved_st = pst->saved_st;
 
-  for(; (c < end) && *c; c++){
+    for(; (c < end) && *c; c++) {
 
-    switch(st){
-
-    case 0/* START */:
-      switch(*c) {
-      case_CR_LF;
-      default: break;
-      }
-      break;
-
-    case_ST_CR(*c);
-
-    case ST_LF:
-    case ST_CRLF:
-      return 0;
-
-    default:
-      DBG("Bad state! st=%i",st);
-      return -99;
+        switch(st){
+        case 0/* START */:
+            switch(*c) {
+            case_CR_LF;
+            default: break;
+            }
+            break;
+        case_ST_CR(*c);
+        case ST_LF:
+        case ST_CRLF:
+            return 0;
+        default:
+            DBG("Bad state! st=%i",st);
+            return -99;
+        } //switch(st)
     }
-  }
 
-  return UNEXPECTED_EOT;
+    return UNEXPECTED_EOT;
 }
 
 static int parse_header_async(sip_header* hdr, parser_state* pst, char* end)
@@ -47,10 +43,10 @@ static int parse_header_async(sip_header* hdr, parser_state* pst, char* end)
     // Header states
     //
     enum {
-	H_NAME=0,
-	H_HCOLON,
-	H_VALUE_SWS,
-	H_VALUE,
+        H_NAME=0,
+        H_HCOLON,
+        H_VALUE_SWS,
+        H_VALUE,
     };
 
     int& st = pst->st;
@@ -59,227 +55,210 @@ static int parse_header_async(sip_header* hdr, parser_state* pst, char* end)
     char** c = &(pst->c);
     char*& begin = pst->beg;
 
-    for(;**c && (*c < end);(*c)++){
+    for(;**c && (*c < end);(*c)++) {
+        switch(st) {
+        case H_NAME:
+            switch(**c) {
+            case_CR_LF;
+            case HCOLON:
+                st = H_VALUE_SWS;
+                hdr->name.set(begin,*c-begin);
+                break;
+            case SP:
+            case HTAB:
+                st = H_HCOLON;
+                hdr->name.set(begin,*c-begin);
+                break;
+            }
+            break;
 
-	switch(st){
+        case H_VALUE_SWS:
+            switch(**c) {
+            case_CR_LF;
 
-	case H_NAME:
-	    switch(**c){
+            case SP:
+            case HTAB:
+                break;
 
-	    case_CR_LF;
+            default:
+                st = H_VALUE;
+                begin = *c;
+                break;
+            }
+            break;
 
-	    case HCOLON:
-		st = H_VALUE_SWS;
-		hdr->name.set(begin,*c-begin);
-		break;
+        case H_VALUE:
+            switch(**c) {
+            case_CR_LF;
+            }
+            break;
 
-	    case SP:
-	    case HTAB:
-		st = H_HCOLON;
-		hdr->name.set(begin,*c-begin);
-		break;
-	    }
-	    break;
+        case H_HCOLON:
+            switch(**c) {
+            case HCOLON:
+                st = H_VALUE_SWS;
+                break;
 
-	case H_VALUE_SWS:
-	    switch(**c){
+            case SP:
+            case HTAB:
+                break;
 
-	    case_CR_LF;
+            default:
+                DBG("Missing ':' after header name");
+                return MALFORMED_SIP_MSG;
+            }
+            break;
 
-	    case SP:
-	    case HTAB:
-		break;
+            case_ST_CR(**c);
 
-	    default:
-		st = H_VALUE;
-		begin = *c;
-		break;
-		
-	    };
-	    break;
+        case ST_LF:
+        case ST_CRLF:
+            switch(saved_st) {
+            case H_NAME:
+                if((*c-(st==ST_CRLF?2:1))-begin == 0) {
+                    //DBG("Detected end of headers");
+                    return 0;
+                }
+                DBG("Illegal CR or LF in header name: <%.*s>",
+                    (int)(*c-begin),begin);
+                return MALFORMED_SIP_MSG;
+            case H_VALUE_SWS:
+                if(!IS_WSP(**c)) {
+                    DBG("Malformed header: <%.*s>",(int)(*c-begin),begin);
+                    return MALFORMED_SIP_MSG;
+                }
+                break;
+            case H_VALUE:
+                if(!IS_WSP(**c)) {
+                    hdr->value.set(begin,(*c-(st==ST_CRLF?2:1))-begin);
+                    //DBG("hdr: \"%.*s: %.*s\"",
+                    //     hdr->name.len,hdr->name.s,
+                    //     hdr->value.len,hdr->value.s);
+                    return 0;
+                }
+                break;
+            default:
+                DBG("Oooops! st=%i", saved_st);
+                break;
+            } //switch(saved_st)
 
-	case H_VALUE:
-	    switch(**c){
-		case_CR_LF;
-	    };
-	    break;
+            st = saved_st;
+            break;
+        } //switch(st)
+    } //for(;**c && (*c < end);(*c)++)
 
-	case H_HCOLON:
-	    switch(**c){
-	    case HCOLON:
-		st = H_VALUE_SWS;
-		break;
-
-	    case SP:
-	    case HTAB:
-		break;
-
-	    default:
-		DBG("Missing ':' after header name");
-		return MALFORMED_SIP_MSG;
-	    }
-	    break;
-
-	case_ST_CR(**c);
-
-	case ST_LF:
-	case ST_CRLF:
-	    switch(saved_st){
-
-	    case H_NAME:
-		if((*c-(st==ST_CRLF?2:1))-begin == 0){
-		    //DBG("Detected end of headers");
-		    return 0;
-		}
- 		DBG("Illegal CR or LF in header name: <%.*s>",
-		    (int)(*c-begin),begin);
- 		return MALFORMED_SIP_MSG;
-
-	    case H_VALUE_SWS:
-		if(!IS_WSP(**c)){
-		    DBG("Malformed header: <%.*s>",(int)(*c-begin),begin);
-		    return MALFORMED_SIP_MSG;
-		}
-		break;
-
-	    case H_VALUE:
-		if(!IS_WSP(**c)){
-		    hdr->value.set(begin,(*c-(st==ST_CRLF?2:1))-begin);
-
-		    //DBG("hdr: \"%.*s: %.*s\"",
-		    //     hdr->name.len,hdr->name.s,
-		    //     hdr->value.len,hdr->value.s);
-		    return 0;
-		}
-		break;
-
-	    default:
-		DBG("Oooops! st=%i",saved_st);
-		break;
-	    }
-
-	    st = saved_st;
-	    break;
-	}
-    }
-
-    switch(st){
-
+    //verify final state
+    switch(st) {
     case H_NAME:
     case H_VALUE:
-	return UNEXPECTED_EOT;
-
+        return UNEXPECTED_EOT;
     case ST_LF:
     case ST_CRLF:
-	switch(saved_st){
-	    
-	case H_NAME:
-	    if((*c-(st==ST_CRLF?2:1))-begin == 0){
-		//DBG("Detected end of headers");
-		return 0;
-	    }
-	    DBG("Illegal CR or LF in header name");
-	    return MALFORMED_SIP_MSG;
-	}
-	break;
-    }
-    
+        switch(saved_st) {
+            case H_NAME:
+                if((*c-(st==ST_CRLF?2:1))-begin == 0) {
+                    //DBG("Detected end of headers");
+                    return 0;
+                }
+                DBG("Illegal CR or LF in header name");
+                return MALFORMED_SIP_MSG;
+        }
+        break;
+    } //switch(st)
+
     DBG("Incomplete header (st=%i;saved_st=%i)",st,saved_st);
     return UNEXPECTED_EOT;
 }
 
 int parse_headers_async(parser_state* pst, char* end)
 {
-  char*& c = pst->c;
-  sip_header* hdr = &(pst->hdr);
+    char*& c = pst->c;
+    sip_header* hdr = &(pst->hdr);
 
-  while(c < end) {
+    while(c < end) {
+        int err = parse_header_async(hdr, pst, end);
+        if(err) return err;
 
-    int err = parse_header_async(hdr, pst, end);
-    if(err) return err;
+        if(hdr->name.len && hdr->value.len) {
+            int type = parse_header_type(hdr);
+            if(type == sip_header::H_CONTENT_LENGTH)
+                str2int(c2stlstr(hdr->value),pst->content_len);
+        }
 
-    if(hdr->name.len && hdr->value.len) {
-      int type = parse_header_type(hdr);
-      if(type == sip_header::H_CONTENT_LENGTH)
-	str2int(c2stlstr(hdr->value),pst->content_len);
+        if(!hdr->name.len && !hdr->value.len) {
+            // end-of-headers
+            return 0;
+        }
+
+        // reset header struct
+        pst->reset_hdr_parser();
     }
 
-    if(!hdr->name.len && !hdr->value.len) {
-      // end-of-headers
-      return 0;
-    }
-
-    // reset header struct
-    pst->reset_hdr_parser();
-  }
-
-  return UNEXPECTED_EOT;
+    return UNEXPECTED_EOT;
 }
 
 int skip_sip_msg_async(parser_state* pst, char* end)
 {
-  enum {
-    ST_FL=0,
-    ST_HDRS,
-    ST_BODY
-  };
+    enum {
+        ST_FL=0,
+        ST_HDRS,
+        ST_BODY
+    };
 
-  int err=0;
+    int err=0;
 
-  char*& c = pst->c;
-  int& stage = pst->stage;
-  /*int& st = pst->st;
-  int& saved_st = pst->saved_st;*/
+    char*& c = pst->c;
+    int& stage = pst->stage;
+    /*int& st = pst->st;
+    int& saved_st = pst->saved_st;*/
 
-  while(c < end) {
+    while(c < end) {
+        switch(stage) {
+        case ST_FL:
+            err = skip_line_async(pst,end);
+            break;
 
-    switch(stage) {
-    case ST_FL:
-      err = skip_line_async(pst,end);
-      break;
+        case ST_HDRS:
+            err = parse_headers_async(pst,end);
+            break;
 
-    case ST_HDRS:
-      err = parse_headers_async(pst,end);
-      break;
+        case ST_BODY:
+            if(!pst->content_len)
+                return 0;
+            if(pst->content_len > end-c) {
+                return UNEXPECTED_EOT;
+            } else {
+                return 0;
+            }
+            break;
 
-    case ST_BODY:
-      if(!pst->content_len)
-	return 0;
-      if(pst->content_len > end-c)
-	return UNEXPECTED_EOT;
-      else
-	return 0;
-      break;
+        default:
+            ERROR("unkown state!!!");
+            return -1;
+        } //switch(stage)
 
-    default:
-      ERROR("unkown state!!!");
-      return -1;
-    }
+        if(!err) {
+            switch(stage) {
+            case ST_FL:
+                stage = ST_HDRS;
+                pst->reset_hdr_parser();
+                break;
+            case ST_HDRS:
+                if(!pst->hdr.name.len && !pst->hdr.value.len) {
+                    // End-of-Header found
+                    stage = ST_BODY;
+                    continue;
+                } else {
+                    // End-of-one-Header
+                    pst->reset_hdr_parser();
+                    continue;
+                }
+                break;
+            }
+        } else {
+            return err;
+        }
+    } //while(c < end)
 
-    if(!err) {
-      switch(stage) {
-      case ST_FL:
-	stage = ST_HDRS;
-	pst->reset_hdr_parser();
-	break;
-      case ST_HDRS:
-	if(!pst->hdr.name.len && !pst->hdr.value.len) {
-	  // End-of-Header found
-	  stage = ST_BODY;
-	  continue;
-	}
-	else {
-	  // End-of-one-Header
-	  pst->reset_hdr_parser();
-	  continue;
-	}
-	break;
-      }
-    }
-    else {
-      return err;
-    }
-  }
-
-  return err;
+    return err;
 }
--- core/sip/sip_parser_async.h
+++ core/sip/sip_parser_async.h
@@ -1,47 +1,44 @@
-#ifndef _sip_parser_async_h_
-#define _sip_parser_async_h_
+#pragma once
 
 #include "parse_header.h"
 
 struct parser_state
 {
-  char* orig_buf;
-  char* c; // cursor
-  char* beg; // last marker for field start
+    char* orig_buf;
+    char* c; // cursor
+    char* beg; // last marker for field start
 
-  int stage;
-  int st; // parser state (within stage)
-  int saved_st; // saved parser state (within stage)
-  sip_header hdr; // temporary header struct
-  
-  int content_len; // detected body content-length
+    int stage;
+    int st; // parser state (within stage)
+    int saved_st; // saved parser state (within stage)
+    sip_header hdr; // temporary header struct
 
-  parser_state()
-    : orig_buf(NULL),c(NULL),beg(NULL),
-      stage(0),st(0),saved_st(0),
-      content_len(0)
-  {}
+    int content_len; // detected body content-length
 
-  void reset(char* buf) {
-    c = orig_buf = buf;
-    reset_hdr_parser();
-    stage = content_len = 0;
-  }
+    parser_state()
+      : orig_buf(NULL),c(NULL),beg(NULL),
+        stage(0),st(0),saved_st(0),
+        content_len(0)
+    {}
 
-  void reset_hdr_parser() {
+    void reset(char* buf) {
+        c = orig_buf = buf;
+        reset_hdr_parser();
+        stage = content_len = 0;
+    }
+
+    void reset_hdr_parser() {
 #pragma GCC diagnostic push
 #pragma GCC diagnostic ignored "-Wclass-memaccess"
-    memset(&hdr,0,sizeof(sip_header));
+        memset(&hdr,0,sizeof(sip_header));
 #pragma GCC diagnostic pop
-    st = saved_st = 0;
-    beg = c;
-  }
+        st = saved_st = 0;
+        beg = c;
+    }
 
-  int get_msg_len() {
-    return c - orig_buf + content_len;
-  }
+    int get_msg_len() {
+        return c - orig_buf + content_len;
+    }
 };
 
 int skip_sip_msg_async(parser_state* pst, char* end);
-
-#endif
