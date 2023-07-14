56b33d37010a0cf99b0fdcb439b6c317a125e442
Michael Furmur <furmur@pm.me>
2023-07-14 10:40:41 +0300

    AmStereoRecorderInfo: fix segfault in del()

--- core/AmStereoRecorderInfo.cpp
+++ core/AmStereoRecorderInfo.cpp
@@ -1,5 +1,22 @@
 #include "AmStereoRecorderInfo.h"
-#include "AmAudioFileRecorderStereoMP3.h"
+#include "AmAudioFileRecorderStereo.h"
+
+void StereoRecordersList::add(const string &recorder_id, int channel_id)
+{
+    for(const auto &i : *this) {
+        if(i.recorder_id == recorder_id && i.channel_id == channel_id)
+            return;
+    }
+    emplace_back(recorder_id,channel_id);
+}
+
+bool StereoRecordersList::del(const string &recorder_id, int channel_id)
+{
+    std::erase_if(*this, [&recorder_id, channel_id](const StereoRecorderInfo &r) -> bool {
+        return r.recorder_id == recorder_id && r.channel_id == channel_id;
+    });
+    return !empty();
+}
 
 void StereoRecordersList::put(unsigned long long  ts, const unsigned char *samples, size_t len, int input_sample_rate)
 {
