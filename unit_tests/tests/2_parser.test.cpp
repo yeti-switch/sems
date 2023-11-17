501eb3a50a9434b02d3606f42cd561f180b203e0
Alexey Volokitin <alexey.v@didww.com>
2023-11-17 15:27:03 +0200

    add unit tests by rfc4475(3.1.1.1, 3.1.1.2, 3.1.1.3)

--- unit_tests/tests/2_parser.test.cpp
+++ unit_tests/tests/2_parser.test.cpp
@@ -3,6 +3,7 @@
 #include <sip/sip_parser.h>
 #include <sip/parse_header.h>
 #include <sip/parse_nameaddr.h>
+#include <sip/parse_from_to.h>
 #include <AmUriParser.h>
 #include <AmSdp.h>
 
@@ -46,6 +47,156 @@ TEST(SipParser, Parsing)
     char data3[] = "GET / SIP/2.0\r\n\r\n";
     msg.copy_msg_buf(data3, strlen(data3));
     ASSERT_NE(parse_sip_msg(&msg, err), EXIT_SUCCESS);
+    msg.release();
+}
+
+// rfc4475 3.1.1.1
+TEST(SipParser, ShortTortuousTest)
+{
+    sip_msg msg;
+    char data[] = "INVITE sip:vivekg@chair-dnrc.example.com;unknownparam SIP/2.0\r\n"
+                  "TO :\r\n"
+                  " sip:vivekg@chair-dnrc.example.com ;   tag    = 1918181833n\r\n"
+                  "from   : \"J Rosenberg \\\\\\\"\"       <sip:jdrosen@example.com>\r\n"
+                  "  ;\r\n"
+                  "  tag = 98asjd8\r\n"
+                  "MaX-fOrWaRdS: 0068\r\n"
+                  "Call-ID: wsinv.ndaksdj@192.0.2.1\r\n"
+                  "Content-Length   : 150\r\n"
+                  "cseq: 0009\r\n"
+                  "  INVITE\r\n"
+                  "Via  : SIP  /   2.0\r\n"
+                  " /UDP\r\n"
+                  "    192.0.2.2;branch=390skdjuw\r\n"
+                  "s :\r\n"
+                  "NewFangledHeader:   newfangled value\r\n"
+                  " continued newfangled value\r\n"
+                  "UnknownHeaderWithUnusualValue: ;;,,;;,;\r\n"
+                  "Content-Type: application/sdp\r\n"
+                  "Route:\r\n"
+                  " <sip:services.example.com;lr;unknownwith=value;unknown-no-value>\r\n"
+                  "v:  SIP  / 2.0  / TCP     spindle.example.com   ;\r\n"
+                  "  branch  =   z9hG4bK9ikj8  ,\r\n"
+                  " SIP  /    2.0   / UDP  192.168.255.111   ; branch=\r\n"
+                  " z9hG4bK30239\r\n"
+                  "m:\"Quoted string \\\"\\\"\" <sip:jdrosen@example.com> ; newparam =\r\n"
+                  "      newvalue ;\r\n"
+                  "  secondparam ; q = 0.33\r\n"
+                  "\r\n"
+                  "v=0\r\n"
+                  "o=mhandley 29739 7272939 IN IP4 192.0.2.3\r\n"
+                  "s=-\r\n"
+                  "c=IN IP4 192.0.2.4\r\n"
+                  "t=0 0\r\n"
+                  "m=audio 49217 RTP/AVP 0 12\r\n"
+                  "m=video 3227 RTP/AVP 31\r\n"
+                  "a=rtpmap:31 LPC";
+    char* err;
+    msg.copy_msg_buf(data, strlen(data));
+    ASSERT_EQ(parse_sip_msg(&msg, err), EXIT_SUCCESS);
+    EXPECT_EQ(msg.from->type, sip_header::H_FROM);
+    EXPECT_EQ(msg.to->type, sip_header::H_TO);
+    EXPECT_EQ(msg.via1->type, sip_header::H_VIA);
+    EXPECT_EQ(msg.callid->type, sip_header::H_CALL_ID);
+    EXPECT_EQ(msg.cseq->type, sip_header::H_CSEQ);
+    EXPECT_EQ(msg.route.size(), 1);
+    EXPECT_EQ(msg.route.back()->type, sip_header::H_ROUTE);
+    EXPECT_EQ(msg.content_length->type, sip_header::H_CONTENT_LENGTH);
+    EXPECT_EQ(msg.content_type->type, sip_header::H_CONTENT_TYPE);
+    EXPECT_EQ(msg.type, SIP_REQUEST);
+    EXPECT_EQ(msg.u.request->method, sip_request::INVITE);
+    EXPECT_EQ(msg.u.request->ruri.scheme, sip_uri::SIP);
+    EXPECT_STREQ(string(msg.u.request->ruri.user.s, msg.u.request->ruri.user.len).c_str(), string("vivekg").c_str());
+    EXPECT_STREQ(string(msg.u.request->ruri.host.s, msg.u.request->ruri.host.len).c_str(), string("chair-dnrc.example.com").c_str());
+    msg.release();
+}
+
+// rfc4475 3.1.1.2
+TEST(SipParser, WideRangeTest)
+{
+    sip_msg msg;
+    char data[] = "!interesting-Method0123456789_*+`.%indeed'~ "
+                    "sip:1_unusual.URI~(to-be!sure)&isn't+it$/crazy?,/;;*:&it+has=1,weird!*pas$wo~d_too.(doesn't-it)"
+                    "@example.com SIP/2.0\r\n"
+                  "Via: SIP/2.0/TCP host1.example.com;branch=z9hG4bK-.!%66*_+`'~\r\n"
+                  "To: \"BEL: NUL: DEL:\" <sip:1_unusual.URI~(to-be!sure)&isn't+it$/crazy?,/;;*@example.com>\r\n"
+                  "From: token1~` token2'+_ token3*%!.- "
+                    "<sip:mundane@example.com>;fromParam''~+*_!.-%=\"работающий\";tag=_token~1'+`*%!-.\r\n"
+                  "Call-ID: intmeth.word%ZK-!.*_+'@word`~)(><:\\/\"][?}{\r\n"
+                  "CSeq: 139122385 !interesting-Method0123456789_*+`.%indeed'~\r\n"
+                  "Max-Forwards: 255\r\n"
+                  "extensionHeader-!.%*+_`'~:大停電\r\n"
+                  "Content-Length: 0\r\n";
+    char* err;
+    msg.copy_msg_buf(data, strlen(data));
+    ASSERT_EQ(parse_sip_msg(&msg, err), EXIT_SUCCESS);
+    EXPECT_EQ(msg.from->type, sip_header::H_FROM);
+    EXPECT_EQ(msg.to->type, sip_header::H_TO);
+    EXPECT_EQ(msg.via1->type, sip_header::H_VIA);
+    EXPECT_EQ(msg.callid->type, sip_header::H_CALL_ID);
+    EXPECT_EQ(msg.cseq->type, sip_header::H_CSEQ);
+    EXPECT_EQ(msg.content_length->type, sip_header::H_CONTENT_LENGTH);
+    EXPECT_EQ(msg.type, SIP_REQUEST);
+    EXPECT_EQ(msg.u.request->method, sip_request::OTHER_METHOD);
+    EXPECT_EQ(msg.u.request->ruri.scheme, sip_uri::SIP);
+    EXPECT_STREQ(string(msg.u.request->method_str.s, msg.u.request->method_str.len).c_str(),
+                 string("!interesting-Method0123456789_*+`.%indeed'~").c_str());
+    EXPECT_STREQ(string(msg.u.request->ruri.user.s, msg.u.request->ruri.user.len).c_str(),
+                 string("1_unusual.URI~(to-be!sure)&isn't+it$/crazy?,/;;*").c_str());
+    EXPECT_STREQ(string(msg.u.request->ruri.passwd.s, msg.u.request->ruri.passwd.len).c_str(),
+                 string("&it+has=1,weird!*pas$wo~d_too.(doesn't-it)").c_str());
+    EXPECT_STREQ(string(msg.u.request->ruri.host.s, msg.u.request->ruri.host.len).c_str(), string("example.com").c_str());
+
+    sip_from_to* to = (sip_from_to*)msg.to->p;
+    sip_from_to* from = (sip_from_to*)msg.from->p;
+    EXPECT_STREQ(string(to->nameaddr.name.s, to->nameaddr.name.len).c_str(),
+                 string("\"BEL: NUL: DEL:\"").c_str());
+    EXPECT_STREQ(string(from->nameaddr.name.s, from->nameaddr.name.len).c_str(),
+                 string("token1~` token2'+_ token3*%!.-").c_str());
+    EXPECT_STREQ(string(from->tag.s, from->tag.len).c_str(),
+                 string("_token~1'+`*%!-.").c_str());
+    msg.release();
+}
+
+// rfc4475 3.1.1.3
+TEST(SipParser, EscapeTest)
+{
+    sip_msg msg;
+    char data[] = "INVITE sip:sips%3Auser%40example.com@example.net SIP/2.0\r\n"
+                  "To: sip:%75se%72@example.com\r\n"
+                  "From: <sip:I%20have%20spaces@example.net>;tag=938\r\n"
+                  "Max-Forwards: 87\r\n"
+                  "Call-ID: esc01.239409asdfakjkn23onasd0-3234\r\n"
+                  "CSeq: 234234 INVITE\r\n"
+                  "Via: SIP/2.0/UDP host5.example.net;branch=z9hG4bKkdjuw\r\n"
+                  "Content-Type: application/sdp\r\n"
+                  "Contact: <sip:cal%6Cer@host5.example.net;%6C%72;n%61me=v%61lue%25%34%31>\r\n"
+                  "Content-Length: 150\r\n"
+                  "\r\n"
+                  "v=0\r\n"
+                  "o=mhandley 29739 7272939 IN IP4 192.0.2.1\r\n"
+                  "s=-\r\n"
+                  "c=IN IP4 192.0.2.1\r\n"
+                  "t=0 0\r\n"
+                  "m=audio 49217 RTP/AVP 0 12\r\n"
+                  "m=video 3227 RTP/AVP 31\r\n"
+                  "a=rtpmap:31 LPC";
+    char* err;
+    msg.copy_msg_buf(data, strlen(data));
+    ASSERT_EQ(parse_sip_msg(&msg, err), EXIT_SUCCESS);
+    EXPECT_EQ(msg.from->type, sip_header::H_FROM);
+    EXPECT_EQ(msg.to->type, sip_header::H_TO);
+    EXPECT_EQ(msg.via1->type, sip_header::H_VIA);
+    EXPECT_EQ(msg.callid->type, sip_header::H_CALL_ID);
+    EXPECT_EQ(msg.cseq->type, sip_header::H_CSEQ);
+    EXPECT_EQ(msg.content_length->type, sip_header::H_CONTENT_LENGTH);
+    EXPECT_EQ(msg.type, SIP_REQUEST);
+    EXPECT_EQ(msg.u.request->method, sip_request::INVITE);
+    EXPECT_EQ(msg.content_type->type, sip_header::H_CONTENT_TYPE);
+    EXPECT_EQ(msg.u.request->ruri.scheme, sip_uri::SIP);
+    EXPECT_STREQ(string(msg.u.request->ruri.user.s, msg.u.request->ruri.user.len).c_str(),
+                 string("sips%3Auser%40example.com").c_str());
+    EXPECT_STREQ(string(msg.u.request->ruri.host.s, msg.u.request->ruri.host.len).c_str(), string("example.net").c_str());
     msg.release();
 }
 
