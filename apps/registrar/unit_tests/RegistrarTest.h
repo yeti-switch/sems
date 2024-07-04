df31b44cbdae0a72dd85c0151994e9a4f69f541a
Michael Furmur <furmur@pm.me>
2024-07-04 17:47:43 +0300

    app/registrar: unit_tests: fix scripts path

--- apps/registrar/unit_tests/RegistrarTest.h
+++ apps/registrar/unit_tests/RegistrarTest.h
@@ -15,10 +15,10 @@ const char aor_lookup_script_hash[] = "af857bc30e9cd6e67f316dc9b0910a19f939f84e"
 const char rpc_aor_lookup_script_hash[] = "91d6959f211b09a6e7b0f1c3c9fd5bf717a371c9";
 const char load_contacts_script_hash[] = "a74bd2c0d28faea0cba58a939af200414ad87ef0";
 
-const char register_script_path[] = "/etc/sems/scripts/register.lua";
-const char aor_lookup_script_path[] = "/etc/sems/scripts/aor_lookup.lua";
-const char rpc_aor_lookup_script_path[] = "/etc/sems/scripts/rpc_aor_lookup.lua";
-const char load_contacts_script_path[] = "/etc/sems/scripts/load_contacts.lua";
+const char register_script_path[] = "apps/registrar/etc/register.lua";
+const char aor_lookup_script_path[] = "apps/registrar/etc/aor_lookup.lua";
+const char rpc_aor_lookup_script_path[] = "apps/registrar/etc/rpc_aor_lookup.lua";
+const char load_contacts_script_path[] = "apps/registrar/etc/load_contacts.lua";
 
 class RegistrarTest : public testing::Test
 {
