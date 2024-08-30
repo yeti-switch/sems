7a369b020da43a64de9a4cc93af534093d3ab9f0
Michael Furmur <furmur@pm.me>
2024-08-30 20:37:25 +0300

    rm unused apps

--- apps/gateway/globals.h
+++
@@ -1,9 +0,0 @@
-#ifndef _GLOBALS_H_
-#define _GLOBALS_H_
-
-#define MODULE_NAME "gateway"
-#define FAIL 0
-#define OK 1
-#define MAX_NUM_LEN 20
-
-#endif 
--- apps/ivr/python_lib.py
+++
@@ -1,3 +0,0 @@
-import distutils.sysconfig
-
-print distutils.sysconfig.get_python_lib(True,True)
