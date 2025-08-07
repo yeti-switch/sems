7f4b2463f8b39999a8211428dbf178c054696bdf
Michael Furmur <furmur@pm.me>
2025-08-07 15:14:49 +0300

    RegistrarClickhouse: adjust logging

--- apps/registrar/RegistrarClickhouse.cpp
+++ apps/registrar/RegistrarClickhouse.cpp
@@ -5,21 +5,20 @@
 #include <AmSessionContainer.h>
 
 RegistrarClickhouse::RegistrarClickhouse()
-  : clickhouse_enable(AmPlugIn::instance()->getFactory4Config("http_client") != nullptr)
+  : clickhouse_enable(false)
 {
 }
 
 int RegistrarClickhouse::configure(cfg_t* cfg)
 {
-    if(!clickhouse_enable){
-        WARN("disable registrar snapshots because http_client module is not loaded");
+    cfg_t* clickhouse = cfg_getsec(cfg, CFG_SEC_CLICKHOUSE);
+    if(!clickhouse) {
+        DBG("disable clickhouse snapshots because of absent clickhouse section in config");
         return 0;
     }
 
-    cfg_t* clickhouse = cfg_getsec(cfg, CFG_SEC_CLICKHOUSE);
-    if(!clickhouse) {
-        WARN("disable registrar snapshots because absent clickhouse section in config");
-        clickhouse_enable = false;
+    if (AmPlugIn::instance()->getFactory4Config("http_client") == nullptr) {
+        WARN("disable clickhouse snapshots because http_client module is not loaded");
         return 0;
     }
 
@@ -27,14 +26,15 @@ int RegistrarClickhouse::configure(cfg_t* cfg)
         clickhouse_dest.push_back(cfg_getnstr(clickhouse, CFG_PARAM_DESTINATIONS, i));
     clickhouse_table = cfg_getstr(clickhouse, CFG_PARAM_TABLE);
     clickhouse_period = cfg_getint(clickhouse, CFG_PARAM_PERIOD);
-    if(clickhouse_dest.size() > 0) {
-        clickhouse_enable = true;
-        snapshots_body_header = "INSERT INTO ";
-        snapshots_body_header += clickhouse_table + " FORMAT JSONEachRow\n";
-    } else {
-        clickhouse_enable = false;
-        WARN("disable registrar snapshots because destinations is empty");
+    if(clickhouse_dest.empty()) {
+        WARN("disable clickhouse snapshots because 'destinations' is empty");
+        return 0;
     }
+
+    snapshots_body_header = "INSERT INTO ";
+    snapshots_body_header += clickhouse_table + " FORMAT JSONEachRow\n";
+
+    clickhouse_enable = true;
 
     return 0;
 }
@@ -57,7 +57,7 @@ void RegistrarClickhouse::on_timer()
     for(unsigned int i = 0;i < snapshot.size();i++)
         data+=arg2json(snapshot[i])+"\n";
 
-    DBG("data:\n%s",data.c_str());
+    //DBG("data:\n%s",data.c_str());
 
     for(const auto &dest: clickhouse_dest) {
         AmSessionContainer::instance()->postEvent(
