3ba8753b9c2bd68e3117c3209498ef0fcd75a288
Michael Furmur <furmur@pm.me>
2025-12-26 15:40:39 +0200

    rm libfmt usage

--- core/format_helper.h
+++ core/format_helper.h
@@ -1,9 +1,2 @@
-#ifndef __cpp_lib_format
-// std::format polyfill using fmtlib
-#define FMT_HEADER_ONLY
-#include <fmt/format.h>
-using fmt::format;
-#else
 #include <format>
 using std::format;
-#endif
