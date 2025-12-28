55ae4e1b0c6ce52771eec4d15cb9cee3bb36e765
Michael Furmur <furmur@pm.me>
2025-12-28 04:20:42 +0200

    Revert "rm libfmt usage"
    This reverts commit 3ba8753b9c2bd68e3117c3209498ef0fcd75a288.

--- core/format_helper.h
+++ core/format_helper.h
@@ -1,2 +1,9 @@
+#ifndef __cpp_lib_format
+// std::format polyfill using fmtlib
+#define FMT_HEADER_ONLY
+#include <fmt/format.h>
+using fmt::format;
+#else
 #include <format>
 using std::format;
+#endif
