c7137eeb4b634642e2a5e4cbd9a77c459b58daa4
Michael Furmur <furmur@pm.me>
2025-12-29 10:43:35 +0200

    format_helper.h: check for __glibcxx_format

--- core/format_helper.h
+++ core/format_helper.h
@@ -1,9 +1,9 @@
-#ifndef __cpp_lib_format
+#if defined __cpp_lib_format || defined __glibcxx_format
+#include <format>
+using std::format;
+#else
 // std::format polyfill using fmtlib
 #define FMT_HEADER_ONLY
 #include <fmt/format.h>
 using fmt::format;
-#else
-#include <format>
-using std::format;
 #endif
